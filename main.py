4d7b3f0dc25f28781642070d92519d810458e060
Pietro Olivetti <72519384+pietroolivetti@users.noreply.github.com>
2023-06-21 22:53:57 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import edge_tts
+from IPython.display import Audio
+import subprocess
+
+st.write("""## TTS anything
+         
+         """)
+txt = st.text_area("Write or paste a text bellow to convert it to audio")
+choice_lang =  ['pt-BR-AntonioNeural', 'en-US-AriaNeural', 'it-IT-DiegoNeural', 'es-AR-ElenaNeural', 'fr-FR-HenriNeural']
+
+# Create a dropdown menu using selectbox
+selected_lang = st.selectbox('Select an option', choice_lang)
+st.write(f'You chose {selected_lang}')
+
+
+if st.button('Convert to audio'):
+    subprocess.run(['edge-tts', '--voice', selected_lang, '--text', txt, '--write-media', 'test.mp3'])
+    st.audio('test.mp3')
+
+#Audio('test.mp3')
