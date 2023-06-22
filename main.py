0a474c454176bfcae2398ed0027b60c182fc05a2
Pietro Olivetti <72519384+pietroolivetti@users.noreply.github.com>
2023-06-22 16:55:53 +0100

    Update main.py
    added speed

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ txt = st.text_area("Write or paste a text bellow to convert it to audio", height
 choice_lang =  {'Português': 'pt-BR-AntonioNeural', 'English': 'en-US-AriaNeural', 'Italiano': 'it-IT-DiegoNeural', 'Español': 'es-AR-ElenaNeural', 'Français': 'fr-FR-HenriNeural'}
 
 # Create a dropdown menu using selectbox
-selected_lang = st.selectbox('Select an option:', choice_lang)
-st.write(f"You've selected: {selected_lang}")
+selected_lang = st.selectbox('Select a language:', choice_lang)
+#st.write(f"You've selected: {selected_lang}")
 
+choice_speed = {'Normal': '0%', 'Very Slow': '-50%', 'Slow': '-25%', 'Fast': '+50%'}
+selected_speed = st.selectbox('Speed: ', choice_speed)
 
 if st.button('Convert to audio'):
-    subprocess.run(['edge-tts', '--voice', choice_lang[selected_lang], '--text', txt, '--write-media', 'test.mp3'])
-    st.audio('test.mp3')
-    
-
+    subprocess.run(['edge-tts', '--rate=' + choice_speed[selected_speed] ,'--voice', choice_lang[selected_lang], '--text', txt, '--write-media', 'test.mp3'])
+    audio = st.audio('test.mp3')
