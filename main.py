19a58537eb8b3c319c21189863498ece7c228062
Pietro Olivetti <72519384+pietroolivetti@users.noreply.github.com>
2023-06-22 18:33:09 +0100

    Update main.py
    added polski and deutsch

--- main.py
+++ main.py
@@ -7,16 +7,15 @@ st.write("""## TTS anything
          """)
 txt = st.text_area("Write or paste a text bellow to convert it to audio", height=300)
 
-#choice_lang =  ['pt-BR-AntonioNeural', 'en-US-AriaNeural', 'it-IT-DiegoNeural', 'es-AR-ElenaNeural', 'fr-FR-HenriNeural']
-choice_lang =  {'Português': 'pt-BR-AntonioNeural', 'English': 'en-US-AriaNeural', 'Italiano': 'it-IT-DiegoNeural', 'Español': 'es-AR-ElenaNeural', 'Français': 'fr-FR-HenriNeural'}
+choice_lang =  {'Português': 'pt-BR-AntonioNeural', 'English': 'en-US-AriaNeural', 'Italiano': 'it-IT-DiegoNeural', 'Español': 'es-AR-ElenaNeural', 'Français': 'fr-FR-HenriNeural', 'Deutsch': 'de-DE-KillianNeural', 'Polski': 'pl-PL-ZofiaNeural'}
 
 # Create a dropdown menu using selectbox
-selected_lang = st.selectbox('Select a language:', choice_lang)
+selected_lang = st.selectbox('Select a language:', choice_lang, index=0)
 #st.write(f"You've selected: {selected_lang}")
 
-choice_speed = {'Normal': '0%', 'Very Slow': '-50%', 'Slow': '-25%', 'Fast': '+50%'}
-selected_speed = st.selectbox('Speed: ', choice_speed)
+choice_speed = {'Normal': '--rate=+1%', 'Very Slow': '--rate=-50%', 'Slow': '--rate=-25%', 'Fast': '--rate=+50%'}
+selected_speed = st.selectbox('Speed: ', choice_speed, index=0)
 
 if st.button('Convert to audio'):
-    subprocess.run(['edge-tts', '--rate=' + choice_speed[selected_speed] ,'--voice', choice_lang[selected_lang], '--text', txt, '--write-media', 'test.mp3'])
-    st.audio('test.mp3')
+    subprocess.run(['edge-tts',  choice_speed[selected_speed] ,'--voice', choice_lang[selected_lang], '--text', txt, '--write-media', 'tts.mp3'])
+    st.audio('tts.mp3')
